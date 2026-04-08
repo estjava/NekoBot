f561ffda36808d3ae0c1a08bb6000452cdc6a8cc
EstJava <sangjendral87@gmail.com>
2026-04-08 22:46:53 +0700

    feat: add prefix configuration and update ping command for async execution

--- src/commands/utility/ping.js
+++ src/commands/utility/ping.js
@@ -3,13 +3,13 @@ module.exports = {
     description: 'Cek latency bot',
     aliases: ['p', 'latency'],
     usage: '!ping',
-    execute(message, args, client) {
-        const sent = message.reply('🏓 Pinging...');
-        sent.then(msg => {
-            const latency = msg.createdTimestamp - message.createdTimestamp;
-            const apiLatency = Math.round(client.ws.ping);
-            
-            msg.edit(`🏓 Pong!\n📡 Latency: ${latency}ms\n💓 API Latency: ${apiLatency}ms`);
-        });
+
+    async execute(message, args, client) {
+        const msg = await message.reply('🏓 Pinging...');
+        
+        const latency = msg.createdTimestamp - message.createdTimestamp;
+        const apiLatency = Math.round(client.ws.ping);
+
+        await msg.edit(`🏓 Pong!\n📡 Latency: ${latency}ms\n💓 API Latency: ${apiLatency}ms`);
     }
 };
